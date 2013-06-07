c76327f9f742722983f99f6d119ba5bea5b9bc66
aaronlsilber <aaronlsilber@gmail.com>
2013-06-07 15:04:57 -0400

    Consecutive events with the same 'label' are now bunched into the same row.

--- data.php
+++ data.php
@@ -1,87 +1,83 @@
 <?php
 
 $data = array();
-$test = array();
 
 $data[] = array(
-  'label' => 'Project 1',
-  'id'    => '015264',
-  'start' => '2013-06-20', 
-  'end'   => '2013-06-26'
+  'label' => 'Aaron Silber',
+  'start' => '2012-04-22', 
+  'end'   => '2012-05-22'
 );
 
 $data[] = array(
+  'label' => 'Aaron Silber',
+  'start' => '2012-05-24', 
+  'end'   => '2012-05-30'
+);
+
+$data[] = array(
+  'label' => 'Sean Schoolcraft',
+  'start' => '2012-05-2', 
+  'end'   => '2012-05-4'
+);
+
+$data[] = array(
+  'label' => 'Sean Schoolcraft',
+  'start' => '2012-05-8', 
+  'end'   => '2012-05-14'
+);
+
+$data[] = array(
+  'label' => 'Sean Schoolcraft',
+  'start' => '2012-05-16', 
+  'end'   => '2012-05-19'
+);
+
+
+
+$data[] = array(
+  'label' => 'Project 4',
+  'start' => '2012-05-06', 
+  'end'   => '2012-06-17',
+  'class' => 'important',
+);
+
+$data[] = array(
   'label' => 'Project 1',
-  'id'    => '015268',
-  'start' => '2013-04-22', 
-  'end'   => '2013-05-22'
+  'start' => '2012-04-20', 
+  'end'   => '2012-05-12'
 );
 
-$test['152648'] = array(
-  '0' => array(
-    'appt_id' => '015268',
-    'start'   => '2013-04-22', 
-    'end'     => '2013-05-22'
-  ),
-  '1' => array(
-    'appt_id' => '015268',
-    'start'   => '2013-04-22', 
-    'end'     => '2013-05-22'
-  ),
+$data[] = array(
+  'label' => 'Project 5',
+  'start' => '2012-05-11', 
+  'end'   => '2012-06-03', 
+  'class' => 'urgent',
 );
 
-dpm($test);
+$data[] = array(
+  'label' => 'Project 6',
+  'start' => '2012-05-15', 
+  'end'   => '2012-07-03'
+);
 
-// $data[] = array(
-//   'label' => 'Project 2',
-//   'start' => '2012-04-18', 
-//   'end'   => '2012-04-20'
-// );
+$data[] = array(
+  'label' => 'Project 7',
+  'start' => '2012-06-01', 
+  'end'   => '2012-07-03', 
+  'class' => 'important',
+);
 
-// $data[] = array(
-//   'label' => 'Project 3',
-//   'start' => '2012-05-25', 
-//   'end'   => '2012-06-20'
-// );
+$data[] = array(
+  'label' => 'Project 8',
+  'start' => '2012-06-01', 
+  'end'   => '2012-08-05'
+);
 
-// $data[] = array(
-//   'label' => 'Project 4',
-//   'start' => '2012-05-06', 
-//   'end'   => '2012-06-17',
-//   'class' => 'important',
-// );
-
-// $data[] = array(
-//   'label' => 'Project 5',
-//   'start' => '2012-05-11', 
-//   'end'   => '2012-06-03', 
-//   'class' => 'urgent',
-// );
-
-// $data[] = array(
-//   'label' => 'Project 6',
-//   'start' => '2012-05-15', 
-//   'end'   => '2012-07-03'
-// );
-
-// $data[] = array(
-//   'label' => 'Project 7',
-//   'start' => '2012-06-01', 
-//   'end'   => '2012-07-03', 
-//   'class' => 'important',
-// );
-
-// $data[] = array(
-//   'label' => 'Project 8',
-//   'start' => '2012-06-01', 
-//   'end'   => '2012-08-05'
-// );
-
-// $data[] = array(
-//   'label' => 'Project 9',
-//   'start' => '2012-07-22', 
-//   'end'   => '2012-09-05',
-//   'class' => 'urgent',
-// );
+$data[] = array(
+  'label' => 'Project 9',
+  'start' => '2012-07-22', 
+  'end'   => '2012-09-05',
+  'class' => 'urgent',
+);
 
 ?>
